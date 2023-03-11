ef2920ccef97866e94d2c8f8eb8647d8ca3e7474
Murat.Can <muratt.can.94@gmail.com>
2023-03-11 20:02:13 +0300

    arraylist

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+
+namespace Arrays
+{
+    class Program
+    {
+        public static void Main (string[] args)
+        {
+            ArrayList liste = new ArrayList();
+
+            liste.Add("Murat");
+            liste.Add(22);
+            liste.Add(true);
+            liste.Add('A');
+
+            Console.WriteLine(liste[1]);
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("*************************");
+            
+            string [] isimler = {"HAKAN","ALEV","MELİH"};
+            List<int> sayilar = new List<int>(){2,5,7,3,8,9};
+            liste.AddRange(isimler);
+            liste.AddRange(sayilar);
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
